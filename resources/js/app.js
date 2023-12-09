267b0b0f90e1be133e11ed4e8cba153568cf6e59
Alex <aluxin@yandex.ru>
2023-12-09 19:18:08 +0800

    Установил пакет плагин tw-elements для Tailwind CSS.

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,13 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+import {
+    Collapse,
+    Dropdown,
+    Ripple,
+    initTE,
+} from "tw-elements";
+
+initTE({ Collapse, Dropdown, Ripple });
