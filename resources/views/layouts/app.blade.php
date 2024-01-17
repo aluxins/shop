160860f8f751f64f58eb4215136f8ec8a9002e84
Aleksey Muhin <aluxin@yandex.ru>
2024-01-17 11:34:57 +0800

    Изменил основной шрифт шаблонов на Fira Sans.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=fira-sans:400,400i,500,500i,600,600i" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
