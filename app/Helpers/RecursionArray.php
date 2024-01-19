174e12083f4d8a951f00225519d17efc1e441e29
Aleksey Muhin <aluxin@yandex.ru>
2024-01-19 16:49:09 +0800

    Переместил рекурсивные функции построения каталога в отдельный класс.

---
+++ app/Helpers/RecursionArray.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Helpers;
+
+if (!class_exists('RecursionArray')) {
+    class RecursionArray {
+        /**
+         * Преобразование одномерного массива в многомерный.
+         * @param array $array
+         * @param int $id
+         * @return array
+         */
+        public static function multidimensional(array $array, int $id = 0): array
+        {
+            $search = [];
+            foreach($array as $el){
+                if($el['parent'] === $id) {
+                    $search[] = [
+                        'id' => $el['id'],
+                        'name' => $el['name'],
+                        'link' => $el['link'],
+                        'children' => self::multidimensional($array, $el['id'])
+                    ];
+                }
+            }
+            return $search;
+        }
+
+        /**
+         * Определение глубины вложенности элемента в одномерном представлении данных.
+         * @param $array
+         * @param $id
+         * @param bool $direction направление: *true - поиск parents* или *false - поиск children
+         * @param bool $line представление выходных данных: *false - одномерный массив* или *true - двумерный массив*
+         * @param array $search
+         * @return array
+         */
+        public static function depth($array, $id, bool $direction = true, bool $line = false, array &$search = []): array
+        {
+            foreach($array as $el){
+                if(($direction ? $el['id'] : $el['parent']) === $id){
+                    $search[] = $line
+                        ? $el['id']
+                        : ['id' => $el['id'], 'name' => $el['name']];
+                    self::depth($array, $direction ? $el['parent'] : $el['id'], $direction, $line,$search);
+                }
+            }
+            return $search;
+        }
+    }
+}
--- app/View/Components/Menu/index.php
+++ app/View/Components/Menu/index.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 use App\Models\StoreSections;
+use App\Helpers\RecursionArray;
 
 class index extends Component
 {
@@ -44,51 +45,11 @@ class index extends Component
     public function __construct(int $idStart = 0, string $type = 'menu', string $selected = '', bool $open = false)
     {
         $array_all = StoreSections::orderBy('sort')->orderBy('id')->get()->toArray();
-        $this->arraySections = $this->recurs($array_all);
-        $this->arrayTree = $this->recursTree($array_all, $idStart);
+        $this->arraySections = RecursionArray::multidimensional($array_all);
+        $this->arrayTree = RecursionArray::depth($array_all, $idStart);
         $this->type = $type;
         $this->selected = $selected;
         $this->open = $open;
-    }
-
-    /**
-     * @param array $array
-     * @param int $id
-     * @return array
-     */
-    public function recurs(array $array, int $id = 0): array
-    {
-        $search = [];
-        foreach($array as $el){
-            if($el['parent'] === $id) {
-                $search[] = [
-                    'id' => $el['id'],
-                    'name' => $el['name'],
-                    'link' => $el['link'],
-                    'children' => self::recurs($array, $el['id'])
-                ];
-            }
-        }
-        return $search;
-    }
-
-    /**
-     * Глубина вложенности разделов.
-     * @param $array
-     * @param $id
-     * @param bool $direction *true - поиск parents* or *false - поиск children
-     * @param array $search
-     * @return array
-     */
-    private function recursTree($array, $id, bool $direction = true, array &$search = []): array
-    {
-        foreach($array as $el){
-            if(($direction ? $el['id'] : $el['parent']) === $id){
-                $search[] = ['id' => $el['id'], 'name' => $el['name']];
-                self::recursTree($array, $direction ? $el['parent'] : $el['id'], $direction, $search);
-            }
-        }
-        return $search;
     }
 
     /**
