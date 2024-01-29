d9e238037481db98b429215de5243ae9d36b5fd0
Aleksey Muhin <aluxin@yandex.ru>
2024-01-29 14:57:25 +0800

    Добавил получение параметров сервера из .env.

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/css/dropdown-menu.css',
-                'resources/js/app.js',
-                'resources/js/dropdown-menu.js',
-                'node_modules/tinymce/tinymce.min.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    resolve: {
-        alias: {
-            '$': 'jQuery'
-        },
-    },
-    server: {
-        host: '192.168.2.10'
-    }
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+	return {
+		plugins: [
+			laravel({
+				input: [
+					'resources/css/app.css',
+					'resources/css/dropdown-menu.css',
+					'resources/js/app.js',
+					'resources/js/dropdown-menu.js',
+					'node_modules/tinymce/tinymce.min.js',
+				],
+				refresh: true,
+			}),
+		],
+		resolve: {
+			alias: {
+				'$': 'jQuery'
+			},
+		},
+		server: {
+			host: 	env.VITE_HOST,
+			port: 	env.VITE_PORT,
+			https: 	env.VITE_HTTPS,
+		}
+	}
 });
